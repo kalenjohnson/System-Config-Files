936cd59815c0fd51186842aedfa68caca8a8d9d4
kalenjohnson <info@kalenjohnson.com>
2014-11-08 22:47:42 -0800

    Added global .gitignore, along with setup command

--- setup.sh
+++ setup.sh
@@ -5,3 +5,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ln -s $DIR/.zshrc $DIR/../.zshrc
 ln -s $DIR/.vimrc $DIR/../.vimrc
 rm -rf $DIR/../.vim; ln -s $DIR/vim $DIR/../.vim
+git config --global core.excludesfile $DIR/.gitignore
